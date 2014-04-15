1cff4b3d99719eb40a133f9ca9b89832b16de2ae
Rhey Anthony Laurente <rhey@Rheys-Mac-mini.local>
2014-04-15 16:26:54 +0100

    draggable features for draggable image

--- photoeditor/public_html/js/modules/editor.js
+++ photoeditor/public_html/js/modules/editor.js
@@ -3,21 +3,21 @@ var editor = angular.module('editor', []);
 editor.factory('pages',function(){
     var pages   = [
         {title:'First page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Second page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Third page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Fourth page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'}
+        {title:'Second page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
+        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'}
     ];
     return pages;
 })
 //  loading page list
-editor.controller('PageListingController',['$scope','pages',function($scope,pages){
+editor.controller('PageListingController',['$scope','pages',function(scope,pages){
     var formatPages =   [];
     var key         =   0;
     pages.forEach(function(entry,id){
@@ -29,5 +29,38 @@ editor.controller('PageListingController',['$scope','pages',function($scope,page
             key++;
         }
     });
-    $scope.pages    =   formatPages;
+    scope.pages        =   formatPages;
+    //  drag photo
+    scope.onDrag       =   function(){
+        alert('hello world');
+    }
+}]);
+// drag image
+editor.directive('imagedrag',[function(){
+    var handleDragStart     =   function(){
+        this.style.opacity  =   0.5;
+    }
+    var handleDragEnd       =   function(){
+        this.style.opacity  =   1;
+    }
+    return {
+        restrict:'A',
+        link:function(scope,jElm,attrs){
+            jElm.attr("draggable","true");
+            jElm.bind("dragstart",handleDragStart);
+            jElm.bind("dragend",handleDragEnd);
+        }
+    }
+}]);
+//  drop image
+editor.directive('imagedrop'[function(){
+    var handleDragOver  =   function(){
+        this.style.border  =   '1px solid red!important';
+    }
+     return{
+        restrict:'A',
+        link: function(scope,jElement,attrs){
+            jElement.bind("dragover",handleDragOver);
+        }
+    }
 }]);
