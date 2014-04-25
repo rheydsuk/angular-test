029f043966cdd0fdd75824c6558a6a1131df05ec
unknown <rhey.laurente@datasouth.co.uk>
2014-04-25 14:07:11 +0800

    bug fixes

--- photoeditor/public_html/js/modules/editor.js
+++ photoeditor/public_html/js/modules/editor.js
@@ -2,65 +2,102 @@ var editor = angular.module('editor', []);
 //  page models
 editor.factory('pages',function(){
     var pages   = [
-        {title:'First page',photo:'http://chunky.llbit.se/images/a/a7/Sample.png'},
-        {title:'Second page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'},
-        {title:'Third page',photo:'http://www.eminenstore.com/wp-content/uploads/2012/04/samples.jpg'}
+        {title:'First page',photo:'images/1.png'},
+        {title:'Second page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'},
+        {title:'Third page',photo:'images/2.jpg'}
     ];
     return pages;
 })
 //  loading page list
 editor.controller('PageListingController',['$scope','pages',function(scope,pages){
-    var formatPages =   [];
-    var key         =   0;
-    pages.forEach(function(entry,id){
-        if(id%2==0){
-            formatPages[key]    =   {};
-            formatPages[key]['left']   =   entry;
-        }else{
-            formatPages[key]['right']   =   entry;
-            key++;
-        }
-    });
-    scope.pages        =   formatPages;
-    //  drag photo
-    scope.onDrag       =   function(){
-        alert('hello world');
+    //  format pages
+    var formatPages     =   function(pages){
+        var formattedPages  =   [];
+        var key         =   0;
+        pages.forEach(function(entry,id){
+            if(id%2==0){
+                formattedPages[key]    =   {};
+                formattedPages[key]['left']   =   entry;
+            }else{
+                formattedPages[key]['right']   =   entry;
+                key++;
+            }
+        });
+        return formattedPages;
     }
+    scope.pages         =   formatPages(pages);
+    //  arrange page
+    scope.arrange   =   function(source,dest){
+        var sourcePage  =   pages[source];
+        var destPage    =   pages[dest];
+        pages[dest]     =   sourcePage;
+        pages[source]   =   destPage;
+        scope.pages     =   formatPages(pages);
+    }    
 }]);
 // drag image
-editor.directive('imagedrag',[function(){
-    var handleDragStart     =   function(){
-        this.style.opacity  =   0.5;
-    }
-    var handleDragEnd       =   function(){
-        this.style.opacity  =   1;
-    }
+editor.directive('imagedrag',function(){
     return {
         restrict:'A',
-        link:function(scope,jElm,attrs){
-            jElm.attr("draggable","true");
-            jElm.bind("dragstart",handleDragStart);
-            jElm.bind("dragend",handleDragEnd);
+        link:function(scope,element,attrs){
+            var el          =   element;
+            el.draggable    =   true;
+            //  drag start handle
+            el.bind('dragstart',function(e) {
+                e.originalEvent.dataTransfer.effectAllowed = 'move';
+                e.originalEvent.dataTransfer.setData('id', this.id);
+                this.classList.add('drag-start');
+            });
+            //  drag end handle
+            el.bind('dragend',function(e){
+                this.classList.remove('drag-start');
+            });
         }
     }
-}]);
+});
 //  drop image
-editor.directive('imagedrop'[function(){
-    var handleDragOver  =   function(){
-        this.style.border  =   '1px solid red!important';
-    }
-     return{
+editor.directive('imagedrop',function(){
+    return {
         restrict:'A',
-        link: function(scope,jElement,attrs){
-            jElement.bind("dragover",handleDragOver);
+        link:function(scope,element,attrs){
+            var el  =   element;
+            //  handle drag over
+            el.bind('dragover',function(e){
+                e.stopPropagation();
+                e.preventDefault();
+                this.classList.add('drag-over');
+            });
+            // handle drag out
+            el.bind('dragleave',function(e){
+                this.classList.remove('drag-over');
+            });
+            // handle drop item
+            el.bind('drop',function(e){
+                e.stopPropagation();
+                e.preventDefault();
+                var source    = e.originalEvent.dataTransfer.getData('id');
+                this.classList.remove('drag-over');
+                if(!source){
+                    var reader      =   new FileReader();
+                    reader.onload   =   function(e){
+                        console.log(e.target.result);
+                    }
+                    reader.readAsDataURL(e.originalEvent.dataTransfer.files[0]);
+                }else{
+                    var dest    =   this.id;
+                    scope.$apply(function(){
+                        scope.arrange(source,dest);
+                    });
+                }
+            });
         }
     }
-}]);
+});
